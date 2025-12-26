3f2a079d9bb6fa2a2fe86ee9e3cb2d8015ba9f80
Leonardo-Franco98 <leonardo.franco28@gmail.com>
2025-12-26 18:28:52 +0000

    fixed lightcone rarity colors not loading properly and lightcone preview image

--- src/pages/lightcones/components/LightconeCard.jsx
+++ src/pages/lightcones/components/LightconeCard.jsx
@@ -1,13 +1,13 @@
 const LightconeCard = ({ lightcone }) => {
 
-  const rarity = lightcone.rarity === 5 ? 'five' : lightcone.rarity === 4 ? 'four' : 'three'
+  const rarityColor = lightcone.rarity === 5 ? 'to-five-stars' : lightcone.rarity === 4 ? 'to-four-stars' : 'to-three-stars'
 
   return (
     <div className="rounded-lg shadow-lg">
-      <div className={`bg-linear-to-br from-slate-700 to-${rarity}stars p-4 rounded-t-lg`}>
-        <img src={`${import.meta.env.VITE_API_URL}/images/lightcone/${lightcone.path} - Preview`} className='h-60 mx-auto' />
+      <div className={`bg-linear-to-br from-slate-700 ${rarityColor} p-4 rounded-t-lg`}>
+        <img src={`${import.meta.env.VITE_API_URL}/images/lightcones/${lightcone.name} - Preview`} className='h-60 mx-auto' />
       </div>
-      <div className="w-full h-auto py-2 flex flex-col justify-between bg-slate-900 px-4 py-8 rounded-b-lg">
+      <div className="w-full h-auto flex flex-col justify-between bg-slate-900 p-8 rounded-b-lg">
         <p className="text-white text-2xl font-semibold mb-4 h-16">{lightcone.name}</p>
         <div>
           <div className="flex items-center gap-4 mb-2">
