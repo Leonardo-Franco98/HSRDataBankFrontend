0bc91ac2e2000b6a23cb38e0f60bb0691f70154f
Leonardo-Franco98 <leonardo.franco28@gmail.com>
2025-12-30 16:14:11 +0000

    improved relic and planar ornament cards

--- src/pages/planarOrnaments/components/PlanarOrnamentCard.jsx
+++ src/pages/planarOrnaments/components/PlanarOrnamentCard.jsx
@@ -1,10 +1,10 @@
 const PlanarOrnamentCard = ({ planarOrnament }) => {
   return (
-    <div className="flex border-solid border-2 min-h-full p-3 gap-6 rounded-md">
+    <div className="flex min-h-full p-3 gap-6 rounded-xl bg-slate-900">
       <img className="w-30 h-30" src={`${import.meta.env.VITE_API_URL}/images/planarOrnaments/${planarOrnament.name.replace(':', ' +')}`} alt="" />
       <div>
-        <p className="text-xl">{planarOrnament.name}</p>
-        <p>2 pieces: {planarOrnament.effect}</p>
+        <p className="text-white text-xl font-semibold mb-2">{planarOrnament.name}</p>
+        <p className="text-white text-md">2 pieces: {planarOrnament.effect}</p>
       </div>
     </div>
   )
--- src/pages/relics/components/RelicCard.jsx
+++ src/pages/relics/components/RelicCard.jsx
@@ -1,11 +1,11 @@
 const RelicCard = ({ relic }) => {
   return (
-    <div className="flex border-solid border-2 min-h-full p-3 gap-6 rounded-md">
+    <div className="flex min-h-full p-3 gap-6 rounded-xl bg-slate-900">
       <img className="w-30 h-30" src={`${import.meta.env.VITE_API_URL}/images/relics/${relic.name}`} alt="" />
       <div>
-        <p className="text-xl">{relic.name}</p>
-        <p>2 pieces: {relic.effect2}</p>
-        <p>4 pieces: {relic.effect4}</p>
+        <p className="text-white text-xl font-semibold mb-2">{relic.name}</p>
+        <p className="text-white text-md">2 pieces: {relic.effect2}</p>
+        <p className="text-white text-md">4 pieces: {relic.effect4}</p>
       </div>
     </div>
   )
